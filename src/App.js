b7bfca823d4b20ac6649edda63fffb5d60c20725
Thang Nguyen <quocthangn1990@gmail.com>
2022-04-12 11:56:18 +0700

    finish-landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Download, Features, SectionWrapper } from "./components";
+import assets from "./assets";
+import styles from "./styles/Global";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper
+        title="Your own store of NFTs. Start Selling & Buying meme in oneclick"
+        description="Collect NFTs, earn rewards by crypto. Join 25+ million people using VNFT Marketplace"
+        showBtn 
+        mockUpImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper
+        title="Friendly User Interface"
+        description="Easy to buy and sell your NFT assests"
+        mockUpImg={assets.homeCards}
+        reverse
+      />
+      <Features/>
+      <SectionWrapper
+        title="Big Rewards"
+        description="When buying NFT, you will receive a random mystery box."
+        mockUpImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="A secure marketplace"
+        description="Your exchange information will be encrypted using advanced technologies"
+        mockUpImg={assets.mockup}
+        banner='banner02'
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made by following the tutorial of {" "}
+          <span className="bold">JavaScriptMastery</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
